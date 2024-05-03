b823908b5558f1c7f28af8d717fc6ca4ebb499b6
chaehyeo-n <parkche03@naver.com>
2024-05-04 01:33:48 +0900

    💄 타이틀 추가

--- FrontEnd/filmme/src/pages/main/Main.jsx
+++ FrontEnd/filmme/src/pages/main/Main.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import * as S from "./style.jsx";
 
 function Main() {
-    return <>요기가 채현이가 작업할 곳!! 파이팅 ㅠㅡㅠ</>;
+    return (
+        <S.MainWrapper>
+            <S.Title>FILM'E</S.Title>
+            <S.subTitle>전국의 독립예술영화관 모아보기</S.subTitle>
+        </S.MainWrapper>
+    );
 }
 
 export default Main;
