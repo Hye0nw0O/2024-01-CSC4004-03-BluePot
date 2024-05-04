12763fc73b858a09981f74bd34dce4c2213f0859
dudtlstm <annpys1024@naver.com>
2024-05-05 02:06:33 +0900

    ✨ feat: footer 색 변경, 아이콘 추가, 프리텐다드 글꼴 전부 추가

--- FrontEnd/filmme/src/components/layouts/footer/Footer.jsx
+++ FrontEnd/filmme/src/components/layouts/footer/Footer.jsx
@@ -3,21 +3,21 @@ import * as S from './style';
 function Footer() {
     return (
         <S.FooterWrapper>
-        <S.FooterLeft>
-            <S.FooterTitle>필름미(FILM'E)</S.FooterTitle>
-            <S.FooterSubTitle>
-            공개SW프로젝트 03분반 3팀 블루팟
-            </S.FooterSubTitle>
-            <S.FooterMiniTitle>
-            @ BluePot. All rights reserved
-            </S.FooterMiniTitle>
-        </S.FooterLeft>
+            <S.FooterLeft>
+                <S.FooterTitle>필름미(FILM'E)</S.FooterTitle>
+                <S.FooterSubTitle>
+                공개SW프로젝트 03분반 3팀 블루팟
+                </S.FooterSubTitle>
+                <S.FooterMiniTitle>
+                @ BluePot. All rights reserved
+                </S.FooterMiniTitle>
+            </S.FooterLeft>
 
-        <S.FooterRight>
-            <S.FooterImg />
-            <S.FooterImg />
-            <S.FooterImg />
-        </S.FooterRight>
+            <S.FooterRight>
+                <S.FooterImg1 />
+                <S.FooterImg2 />
+                <S.FooterImg3 />
+            </S.FooterRight>
         </S.FooterWrapper>
     );
 }
