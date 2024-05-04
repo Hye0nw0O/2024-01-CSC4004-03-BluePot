25f0781e86c1e7bee5d5c3255f0b73b96369e4d2
dudtlstm <annpys1024@naver.com>
2024-05-05 02:07:01 +0900

    ✨ feat: navbar 선택 시 색상 변경되도록 수정

--- FrontEnd/filmme/src/components/layouts/navbar/NavBar.jsx
+++ FrontEnd/filmme/src/components/layouts/navbar/NavBar.jsx
@@ -1,18 +1,38 @@
 import * as S from './style';
 import Logo from '../../../../public/logo.svg';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 function NavBar() {
+    let navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleOutsideClick = event => {
+            if (isOpen && !event.target.closest('#navbar')) {
+            setIsOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+        }, [isOpen]);
+    
+
     return (
     <S.Nav>
         <S.Logo>
             <img src={Logo} alt="로고" />
         </S.Logo>
 
-        <S.NavTabWrapper>
-            <S.NavTab to="/">영화관 소개</S.NavTab>
-            <S.NavTab to="/community">커뮤니티</S.NavTab>
-            <S.NavTab to="/about">서비스 소개</S.NavTab>
-            <S.NavTab to="/auths">로그인</S.NavTab>
+        <S.NavTabWrapper isOpen={isOpen}>
+            <S.NavTab to="/" isOpen={isOpen}>영화관 소개</S.NavTab>
+            <S.NavTab to="/community" isOpen={isOpen}>커뮤니티</S.NavTab>
+            <S.NavTab to="/about" isOpen={isOpen}>서비스 소개</S.NavTab>
+            <S.NavTab to="/auths" isOpen={isOpen}>로그인</S.NavTab>
         </S.NavTabWrapper>
     </S.Nav>
     );
--- FrontEnd/filmme/src/components/layouts/navbar/style.jsx
+++ FrontEnd/filmme/src/components/layouts/navbar/style.jsx
@@ -12,28 +12,57 @@ export const Nav = styled.nav`
     align-items: center;
     flex-shrink: 0;
     font-family: Pretendard-Medium;
+    background-color: #fff;
 `;
 
 export const Logo = styled.div`
     display: flex;
+    width: 60%;
+    height: 60%;
+
+    @media screen and (max-width: 768px) {
+        width: 30%;
+        height: 30%;
+    }
 `;
 
 export const NavTabWrapper = styled.div`
-    display: flex;
     display: flex;
     align-items: flex-start;
     gap: 40px;
     font-size: 20px;
     font-weight: 700;
     line-height: 24px;
+
+    @media screen and (max-width: 768px) {
+        display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+        flex-direction: column;
+        width: 160px;
+        min-height: 125px;
+        position: fixed;
+        top: 72px;
+        right: 10px;
+        background-color: #fff;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        z-index: 20;
+        align-items: center;
+        padding: 10px;
+        gap: 20px;
+        border-radius: 10px;
+    }
 `;
 
 export const NavTab = styled(NavLink)`
     display: flex;
-    font-size: 20px;
+    font-size: ${({ isOpen }) => (isOpen ? '16px' : '20px')};
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-decoration-line: none;
     color: #343030;
+
+    &.active {
+        font-family: Pretendard-Bold;
+        color: #161835;
+    }
 `;
