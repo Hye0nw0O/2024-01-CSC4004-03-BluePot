4fec674cf5cadaf5537c19657789d42bb3dce562
Hye0nw0O <hw4151@naver.com>
2024-05-07 15:28:17 +0900

    ✨feat: url settings

--- BackEnd/filmme/main/urls.py
+++ BackEnd/filmme/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
+from .views import *
+
 urlpatterns = [
-    
+    path('cinemas/', Cinema_List.as_view(), name = 'cinema-list'),
+    path('cinemas_star/', Star_Cinema_List.as_view(), name = 'star_cinema'),
 ]
