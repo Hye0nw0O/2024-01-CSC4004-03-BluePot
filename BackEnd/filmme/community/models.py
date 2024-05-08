d03fa333410c5b41305fba925489924c1e07f1e6
seop637 <smile10316@naver.com>
2024-05-09 03:48:18 +0900

    ✨feat: 커뮤니티 모델 설계

--- BackEnd/filmme/community/models.py
+++ BackEnd/filmme/community/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from accounts import models
-
+from users.models import User
 
 # Create your models here.
 def community_image_upload_path(instance, filename):
@@ -17,8 +17,6 @@ class Community(models.Model):
     )
     category = models.CharField(max_length=10, choices=CATEGORY_LIST, blank=False, null=False)
     #writer = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
-    id3 = models.AutoField()
-    id22 = models.AutoField()
     title = models.CharField(max_length=30)
     content = models.TextField(null=False, max_length=5000)
     view_cnt = models.PositiveIntegerField(default=0)
@@ -54,3 +52,16 @@ class CommunityLike(models.Model):
     #user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, related_name='likes_community')
     user_id = models.AutoField()
     community_id = models.AutoField()
+
+class CommunityPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards")
+    title = models.CharField("제목", max_length=50, null=False)
+    content = models.TextField("내용", null=False)
+    dt_created = models.DateTimeField("작성일", auto_now_add=True, null=False)
+    dt_modified = models.DateTimeField("수정일", auto_now=True, null=False)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'comment' )
