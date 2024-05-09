1800ceb8cf621f03b054098b273c312cadef0768
chaehyeo-n <parkche03@naver.com>
2024-05-09 23:16:51 +0900

    💄 영화관 리스트 추가

---
+++ FrontEnd/filmme/src/data/theater.js
@@ -0,0 +1,67 @@
+import theater1 from "../assets/images/Main/theater/에무시네마.png";
+import theater2 from "../assets/images/Main/theater/라이카시네마.png";
+import theater3 from "../assets/images/Main/theater/인디스페이스.png";
+
+const theater = [
+    {
+        id: 1,
+        name: '에무시네마',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater1,
+    },
+
+    {
+        id: 2,
+        name: '라이카시네마',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater2,
+    },
+
+    {
+        id: 3,
+        name: '인디스페이스',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater3,
+    },
+
+    {
+        id: 4,
+        name: '하하하',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater3,
+    },
+
+    {
+        id: 5,
+        name: '이것도',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater1,
+    },
+
+    {
+        id: 6,
+        name: '머지할것이다!!!',
+        region: '서울',
+        star: '⭐⭐⭐⭐⭐',
+        score: '(42)',
+        like: '102',
+        img: theater2,
+    },
+];
+
+export default theater;
