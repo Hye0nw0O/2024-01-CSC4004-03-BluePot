39c7761a40494a37da1662dfd69c3ed22e948eba
dudtlstm <annpys1024@naver.com>
2024-05-16 12:47:45 +0900

    ✨ feat: added mypage title

--- FrontEnd/filmme/src/pages/mypage/Mypage.jsx
+++ FrontEnd/filmme/src/pages/mypage/Mypage.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import * as S from "./style.jsx";
 
 function Mypage() {
-    return <>마이 페이지</>;
+
+    // const [currentTab, setCurrentTab] = useState(0);
+
+    // const tabContents = [
+    //     <CommunityCommon />,
+    //     <CommunityReview />,
+    //     <CommunitySuggestion />
+    // ];
+
+    // const selectMenuHandler = index => {
+    //     setCurrentTab(index);
+    // }
+    
+    return (
+        <S.MypageWrapper>
+            <S.MypageHeaderWrapper>
+                <S.MypageTitle>Mypage</S.MypageTitle>
+                <S.MypageSubTitle>어쩌구님의 마이페이지</S.MypageSubTitle>
+            </S.MypageHeaderWrapper>
+        </S.MypageWrapper>
+    );
 }
 
 export default Mypage;
