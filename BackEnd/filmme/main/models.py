13dd153a8286832cc51647fe40bc17e9e58b0a48
Hye0nw0O <hw4151@naver.com>
2024-05-30 15:09:53 +0900

    ✨ feat: 영화관에 상영중인 영화 리스트 정보 출력

--- BackEnd/filmme/main/models.py
+++ BackEnd/filmme/main/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    id = models.AutoField(primary_key=True)
-    cinema = models.CharField(max_length=100)
-    name = models.CharField(max_length=15)
 
 class Cinema(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,5 +10,15 @@ class Cinema(models.Model):
     star = models.DecimalField(max_digits=2, decimal_places=1)
     like_cnt = models.IntegerField(default=0)
     location = models.CharField(max_length=50, default='')
+    latitude = models.FloatField(default=37.579472)
+    longitude = models.FloatField(default=126.976872)
     class Meta:
         db_table = 'Cinema'
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    cinema = models.ForeignKey(Cinema, related_name='movies', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    poster_url = models.URLField(max_length=200, blank=True)
+    class Meta:
+        db_table = 'Movie'
