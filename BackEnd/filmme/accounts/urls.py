93c2d93801c3404868aeb1cf9ec11b6d7380c824
lhj010217 <eslee1777@naver.com>
2024-05-31 17:50:25 +0900

    ✨ feat : authorize API

--- BackEnd/filmme/accounts/urls.py
+++ BackEnd/filmme/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('accounts/kakao/logout', kakao_logout, name='kakao_logout'),
     path('accounts/kakao/logout_callback',kakao_logout_callback, name='kakao_logout_callback'),
     path('accounts/kakao/delete', kakao_delete, name='kakao_delete'),
-    #path('accounts/userProfile', user_profile, name='user_profile'),
+    path('accounts/auth', auth, name='auth')
 ]
