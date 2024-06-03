cd9ab7a41f725618f32bf26caeda99ce8df4d1b6
chaehyeo-n <parkche03@naver.com>
2024-06-04 02:52:56 +0900

    🐛 fix: 메인페이지 api 연동

--- FrontEnd/filmme/src/data/theater.jsx
+++ FrontEnd/filmme/src/data/theater.jsx
@@ -9,7 +9,7 @@ import theater3 from "../assets/images/Main/theater/인디스페이스.png";
 const Theater = () => {
     const [theaters, setTheaters] = useState([]);
     useEffect(() => {
-      axios.get('http://localhost:8000/cinemas/')
+      axios.get('http://localhost:8000/api/cinemas/')
         .then(response => {
           console.log(response);
           console.log(response.data);
@@ -37,67 +37,5 @@ const Theater = () => {
         </div>
       );
     }
-
-/*const theater = [
-    {
-        id: 1,
-        name: '머지할것이다!!!',
-        region: '세종',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 112,
-        img: theater2,
-    },
-
-    {
-        id: 2,
-        name: '이것도',
-        region: '대전',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 110,
-        img: theater1,
-    },
-
-    {
-        id: 3,
-        name: '하하하',
-        region: '강원',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 100,
-        img: theater3,
-    },
-
-    {
-        id: 4,
-        name: '에무시네마',
-        region: '서울',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 99,
-        img: theater1,
-    },
-
-    {
-        id: 5,
-        name: '라이카시네마',
-        region: '인천',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 102,
-        img: theater2,
-    },
-
-    {
-        id: 6,
-        name: '인디스페이스',
-        region: '경기',
-        star: '⭐⭐⭐⭐⭐',
-        score: '(42)',
-        like: 101,
-        img: theater3,
-    },
-];*/
 
 export default Theater;
