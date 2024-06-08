e6e9826159eb0ac3a2f5c2741c9908f403bacaeb
chaehyeo-n <parkche03@naver.com>
2024-06-08 17:20:03 +0900

    🔥 remove: 기존 영화관 이미지 삭제

--- FrontEnd/filmme/src/data/theater.jsx
+++ FrontEnd/filmme/src/data/theater.jsx
@@ -2,10 +2,6 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Card from '../components/card/Card';
 
-import theater1 from "../assets/images/Main/theater/에무시네마.png";
-import theater2 from "../assets/images/Main/theater/라이카시네마.png";
-import theater3 from "../assets/images/Main/theater/인디스페이스.png";
-
 const Theater = () => {
     const [theaters, setTheaters] = useState([]);
     useEffect(() => {
