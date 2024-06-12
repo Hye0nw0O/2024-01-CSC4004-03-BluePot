b48ce0a8bd8bd24507b64ff229775937e778da60
chaehyeo-n <parkche03@naver.com>
2024-06-13 02:01:44 +0900

    🐛 fix: 좋아요 기능 새로고침시 생기는 오류 수정

--- FrontEnd/filmme/src/components/card/Card.jsx
+++ FrontEnd/filmme/src/components/card/Card.jsx
@@ -4,56 +4,57 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import axios from 'axios';
 
-function Card({ id, img, name, region, star, score, like, onClick, onLikeToggle, isLiked }) {
+function Card({ id, img, name, region, star, score, like, onClick, onLikeToggle }) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(like);
   const [isLikeButtonDisabled, setIsLikeButtonDisabled] = useState(false);
 
   useEffect(() => {
     AOS.init({
-      duration : 500
+      duration: 500
     });
   }, []);
 
   const handleLikeClick = async (e) => {
-    e.stopPropagation(); // 이벤트 버블링 방지
-    e.preventDefault(); // 새로 고침 방지
-    if (isLikeButtonDisabled) return; // 버튼이 이미 비활성화되어 있으면 요청을 보내지 않음
-    setIsLikeButtonDisabled(true); // 버튼 비활성화
+    e.stopPropagation();
+    e.preventDefault();
+    if (isLikeButtonDisabled) return;
+    setIsLikeButtonDisabled(true);
     try {
       const response = await axios.post(`http://localhost:8000/api/cinemas/like/${id}/`);
       if (response.status === 200) {
-        const updatedLikeCount = response.data.like_cnt;
-        console.log(updatedLikeCount);
-        onLikeToggle(id, updatedLikeCount); // 좋아요 토글 이벤트 호출 및 업데이트된 좋아요 수 전달
+        setLikeCount(prevCount => prevCount + 1); // 좋아요 수 증가
+        setIsLiked(true);
       } else {
         console.error("좋아요를 업데이트하는 데 문제가 발생했습니다.");
       }
     } catch (error) {
       console.error("서버 요청 중 오류가 발생했습니다:", error);
     } finally {
-      setIsLikeButtonDisabled(false); // 요청이 완료되면 버튼 활성화
+      setIsLikeButtonDisabled(false);
     }
   };
 
   return (
-    <>
-      <S.CardWrapper data-aos="fade-up" onClick={onClick}>
-        <S.CardImg src={img} />
-        <S.CardContent>
-          <S.CardContentInfo>
-            <S.CardContentName>{name}</S.CardContentName>
-            <S.CardContentRegion region={region}>{region}</S.CardContentRegion>
-          </S.CardContentInfo>
-          <S.CardContentInfo>
-            <S.CardContentStar>{star}</S.CardContentStar>
-            <S.CardContentScore>({score})</S.CardContentScore>
-          </S.CardContentInfo>
-          <S.CardContentInfo>
-            <S.CardLikehart onClick={handleLikeClick} disabled={isLikeButtonDisabled}>{isLiked ? '❤️' : '♡'}</S.CardLikehart>
-            <S.CardContentLike>{like}</S.CardContentLike>
-          </S.CardContentInfo>
-        </S.CardContent>
-      </S.CardWrapper>
-    </>
+    <S.CardWrapper data-aos="fade-up" onClick={onClick}>
+      <S.CardImg src={img} />
+      <S.CardContent>
+        <S.CardContentInfo>
+          <S.CardContentName>{name}</S.CardContentName>
+          <S.CardContentRegion region={region}>{region}</S.CardContentRegion>
+        </S.CardContentInfo>
+        <S.CardContentInfo>
+          <S.CardContentStar>{star}</S.CardContentStar>
+          <S.CardContentScore>({score})</S.CardContentScore>
+        </S.CardContentInfo>
+        <S.CardContentInfo>
+          <S.CardLikehart onClick={handleLikeClick} disabled={isLikeButtonDisabled}>
+            {isLiked ? '❤️' : '♡'}
+          </S.CardLikehart>
+          <S.CardContentLike>{likeCount}</S.CardContentLike>
+        </S.CardContentInfo>
+      </S.CardContent>
+    </S.CardWrapper>
   );
 }
 
